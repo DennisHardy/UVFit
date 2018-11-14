ebcdbb97fd80e06cddbe691ee52958f2c1e259b7
Ryan Sims <ryanjsims@email.arizona.edu>
2018-11-14 01:37:04 +0000

    Added activities to home page

--- eceServer18/public/uvfit/javascripts/status.js
+++ eceServer18/public/uvfit/javascripts/status.js
@@ -1,39 +1,76 @@
 function sendReqForAccountInfo() {
-   $.ajax({
-      url: '/users/account',
-      type: 'GET',
-      headers: { 'x-auth': window.localStorage.getItem("authToken") },
-      responseType: 'json',
-      success: accountInfoSuccess,
-      error: accountInfoError
-   });
+    $.ajax({
+        url: '/users/account',
+        type: 'GET',
+        headers: { 'x-auth': window.localStorage.getItem("authToken") },
+        responseType: 'json',
+        success: accountInfoSuccess,
+        error: accountInfoError
+    });
+}
+
+function sendReqForActivityInfo() {
+    $.ajax({
+        url: '/activity/all',
+        type: 'GET',
+        headers: { 'x-auth': window.localStorage.getItem("authToken") },
+        responseType: 'json',
+        success: activitySuccess,
+        error: activityError
+    });
+}
+
+function activitySuccess(data, textSatus, jqXHR) {
+    
+    $("#main").show();
+
+    // Add the devices to the list before the list item for the add device button (link)
+    for (var activity of data.activities) {
+        $("#addActivityForm").before("<li class='collection-item'>ID: " 
+            + "<a href='activity.html?id=" + activity.activityId + "'>" 
+            + activity.activityId + "</a>" + ", Start: " + activity.startTime + "</li>")
+    }
+}
+
+function activityError(jqXHR, textStatus, errorThrown) {
+    // If authentication error, delete the authToken 
+    // redirect user to sign-in page (which is index.html)
+    if( jqXHR.status === 401 ) {
+        console.log("Invalid auth token");
+        window.localStorage.removeItem("authToken");
+        window.location.replace("index.html");
+    } 
+    else {
+        $("#error").html("Error: " + status.message);
+        $("#error").show();
+    } 
 }
 
 function accountInfoSuccess(data, textSatus, jqXHR) {
-   $("#email").html(data.email);
-   $("#fullName").html(data.fullName);
-   $("#lastAccess").html(data.lastAccess);
-   $("#main").show();
-   
-   // Add the devices to the list before the list item for the add device button (link)
-   for (var device of data.devices) {
-      $("#addDeviceForm").before("<li class='collection-item'>ID: " +
-        device.deviceId + ", APIKEY: " + device.apikey + "</li>")
-   }
+    $("#email").html(data.email);
+    $("#fullName").html(data.fullName);
+    $("#lastAccess").html(data.lastAccess);
+    $("#main").show();
+
+    // Add the devices to the list before the list item for the add device button (link)
+    for (var device of data.devices) {
+        $("#addDeviceForm").before("<li class='collection-item'>ID: " +
+            device.deviceId + ", APIKEY: " + device.apikey + "</li>")
+    }
 }
 
 function accountInfoError(jqXHR, textStatus, errorThrown) {
-   // If authentication error, delete the authToken 
-   // redirect user to sign-in page (which is index.html)
-   if( jqXHR.status === 401 ) {
-      console.log("Invalid auth token");
-      window.localStorage.removeItem("authToken");
-      window.location.replace("index.html");
-   } 
-   else {
-     $("#error").html("Error: " + status.message);
-     $("#error").show();
-   } 
+    // If authentication error, delete the authToken 
+    // redirect user to sign-in page (which is index.html)
+    if( jqXHR.status === 401 ) {
+        console.log("Invalid auth token");
+        window.localStorage.removeItem("authToken");
+        window.location.replace("index.html");
+    } 
+    else {
+        $("#error").html("Error: " + status.message);
+        $("#error").show();
+    } 
 }
 
 // Registers the specified device with the server.
@@ -45,10 +82,10 @@ function registerDevice() {
         data: { deviceId: $("#deviceId").val() }, 
         responseType: 'json',
         success: function (data, textStatus, jqXHR) {
-           // Add new device to the device list
-           $("#addDeviceForm").before("<li class='collection-item'>ID: " +
-           $("#deviceId").val() + ", APIKEY: " + data["apikey"] + "</li>")
-           hideAddDeviceForm();
+            // Add new device to the device list
+            $("#addDeviceForm").before("<li class='collection-item'>ID: " +
+                $("#deviceId").val() + ", APIKEY: " + data["apikey"] + "</li>")
+            hideAddDeviceForm();
         },
         error: function(jqXHR, textStatus, errorThrown) {
             var response = JSON.parse(jqXHR.responseText);
@@ -60,31 +97,32 @@ function registerDevice() {
 
 // Show add device form and hide the add device button (really a link)
 function showAddDeviceForm() {
-   $("#deviceId").val("");           // Clear the input for the device ID
-   $("#addDeviceControl").hide();    // Hide the add device link
-   $("#addDeviceForm").slideDown();  // Show the add device form
+    $("#deviceId").val("");           // Clear the input for the device ID
+    $("#addDeviceControl").hide();    // Hide the add device link
+    $("#addDeviceForm").slideDown();  // Show the add device form
 }
 
 // Hides the add device form and shows the add device button (link)
 function hideAddDeviceForm() {
-   $("#addDeviceControl").show();  // Hide the add device link
-   $("#addDeviceForm").slideUp();  // Show the add device form
-   $("#error").hide();
+    $("#addDeviceControl").show();  // Hide the add device link
+    $("#addDeviceForm").slideUp();  // Show the add device form
+    $("#error").hide();
 }
 
 // Handle authentication on page load
 $(function() {
-   // If there's no authToekn stored, redirect user to 
-   // the sign-in page (which is index.html)
-   if (!window.localStorage.getItem("authToken")) {
-      window.location.replace("index.html");
-   }
-   else {
-      sendReqForAccountInfo();
-   }
-   
-   // Register event listeners
-   $("#addDevice").click(showAddDeviceForm);
-   $("#registerDevice").click(registerDevice);   
-   $("#cancel").click(hideAddDeviceForm);   
+    // If there's no authToekn stored, redirect user to 
+    // the sign-in page (which is index.html)
+    if (!window.localStorage.getItem("authToken")) {
+        window.location.replace("index.html");
+    }
+    else {
+        sendReqForAccountInfo();
+        sendReqForActivityInfo();
+    }
+
+    // Register event listeners
+    $("#addDevice").click(showAddDeviceForm);
+    $("#registerDevice").click(registerDevice);   
+    $("#cancel").click(hideAddDeviceForm);   
 });
